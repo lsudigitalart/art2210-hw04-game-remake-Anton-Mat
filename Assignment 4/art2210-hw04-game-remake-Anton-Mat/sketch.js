0e18a8036f31e20cd4436e638b10a521df8d6710
Anton-Mat <amatyl1@lsu.edu>
2025-09-29 15:25:51 -0500

    Add files via upload

---
+++ Assignment 4/art2210-hw04-game-remake-Anton-Mat/sketch.js
@@ -0,0 +1,137 @@
+let playerX, playerY;
+let playerSize = 40;
+let playerSpeed = 6;
+let lives = 5;
+let enemies = [];
+let enemyBullets = [];
+let gameState = "playing";
+let lastEnemyAddTime = 0;
+let startTime = 0;
+let elapsedTime = 0;
+
+function setup() {
+  createCanvas(800, 600);
+  playerX = width / 2;
+  playerY = height - 50;
+
+
+  for (let i = 0; i < 5; i++) {
+    addEnemy();
+  }
+  lastEnemyAddTime = millis();
+  startTime = millis();
+}
+
+function draw() {
+  background(20, 20, 40);
+
+  if (gameState === "playing") {
+    playGame();
+  } else {
+    drawGameOver();
+  }
+}
+
+function playGame() {
+ 
+  elapsedTime = Math.floor((millis() - startTime) / 1000);
+
+
+  if (keyIsDown(LEFT_ARROW) && playerX > 0) playerX -= playerSpeed;
+  if (keyIsDown(RIGHT_ARROW) && playerX < width - playerSize) playerX += playerSpeed;
+
+
+  fill(100, 200, 255);
+  rect(playerX, playerY, playerSize, playerSize);
+
+ 
+  for (let e of enemies) {
+    e.x += e.speed;
+    if (e.x < e.size / 2 || e.x > width - e.size / 2) e.speed *= -1;
+
+
+    fill(255, 100, 100);
+    ellipse(e.x, e.y, e.size);
+
+   
+    if (millis() - e.lastShot > e.shootDelay) {
+      enemyBullets.push({ x: e.x, y: e.y, size: 10, speed: 5 });
+      e.lastShot = millis();
+      e.shootDelay = random(500, 1500); // random delay
+    }
+  }
+
+ 
+  for (let i = enemyBullets.length - 1; i >= 0; i--) {
+    let b = enemyBullets[i];
+    b.y += b.speed;
+    fill(255, 255, 0);
+    ellipse(b.x, b.y, b.size);
+
+    
+    if (b.y > height) {
+      enemyBullets.splice(i, 1);
+      continue;
+    }
+
+ 
+    if (b.x > playerX && b.x < playerX + playerSize &&
+        b.y > playerY && b.y < playerY + playerSize) {
+      lives--;
+      enemyBullets.splice(i, 1);
+      if (lives <= 0) gameState = "gameOver";
+    }
+  }
+
+ 
+  if (millis() - lastEnemyAddTime > 10000) {
+    addEnemy();
+    lastEnemyAddTime = millis();
+  }
+
+
+  fill(255);
+  textSize(20);
+  text("Lives: " + lives, 20, 30);
+  text("Enemies: " + enemies.length, 20, 60);
+  text("Time: " + elapsedTime + "s", 20, 90);
+}
+
+function addEnemy() {
+  enemies.push({
+    x: random(100, width - 100),
+    y: 60,
+    size: 40,
+    speed: random([-2, 2]),
+    lastShot: millis(),
+    shootDelay: random(500, 1500)
+  });
+}
+
+function drawGameOver() {
+  background(80, 0, 0);
+  fill(255);
+  textAlign(CENTER);
+  textSize(36);
+  text("GAME OVER", width / 2, height / 2 - 20);
+  textSize(24);
+  text("You survived " + elapsedTime + " seconds!", width / 2, height / 2 + 20);
+  textSize(18);
+  text("Press R to restart", width / 2, height / 2 + 60);
+}
+
+function keyPressed() {
+  if (gameState === "gameOver" && key === 'r') resetGame();
+}
+
+function resetGame() {
+  gameState = "playing";
+  lives = 5;
+  playerX = width / 2;
+  enemies = [];
+  enemyBullets = [];
+  for (let i = 0; i < 5; i++) addEnemy();
+  lastEnemyAddTime = millis();
+  startTime = millis();
+  elapsedTime = 0;
+}
